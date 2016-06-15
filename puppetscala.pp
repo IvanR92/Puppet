05b2c628aa717867ec65dd4e0b7dcefb546d7e1a
ivanr92 <ivanr92@bk.ru>
2016-06-15 15:22:06 +0300

    заменил папет скрипт

--- puppetscala.pp
+++ puppetscala.pp
@@ -8,36 +8,5 @@ node default {
     version => '8u45',
     type    => 'jdk'
   }
-  class scala {
-  include wget
-
-  validate_re($::osfamily, '^Debian$', "This module relies on a deb package \
-and only works on Debian based distros")
-
-  wget::fetch { 'download-typesafe-repo':
-    source      => 'http://apt.typesafe.com/repo-deb-build-0002.deb',
-    destination => '/usr/local/src/repo-deb-build-0002.deb',
-    before      => Package['typesafe-repo'],
-  }
-
-  package { 'typesafe-repo':
-    ensure   => installed,
-    source   => '/usr/local/src/repo-deb-build-0002.deb',
-    provider => dpkg,
-    notify   => Exec['apt-update-typesafe'];
-  }
-
-  exec { 'apt-update-typesafe':
-    command     => '/usr/bin/apt-get update',
-    refreshonly => true;
-  }
-
-  package { [
-    'typesafe-stack',
-    'scala',
-  ]:
-    ensure  => installed,
-    require => Exec['apt-update-typesafe'],
-  }
-}
+  class {'scala': }
 }
